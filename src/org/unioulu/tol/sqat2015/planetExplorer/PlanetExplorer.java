d19a79f96440a05c5ec2991a4f7ae26503b322b9
somename <someemail>
2015-10-23 12:35:19 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -58,7 +58,7 @@ public class PlanetExplorer {
 	public void parsingExplorerPosition(String position){
 		String[] separated = position.split("[(,)]");
 		System.out.println(separated.length);
-		System.out.println(separated[0]);
+		System.out.println(separated[1]);
 		System.out.println(separated[3]);
 		System.out.println(separated[5]);
 		this.explorerPositionX = Integer.parseInt(separated[1]);
