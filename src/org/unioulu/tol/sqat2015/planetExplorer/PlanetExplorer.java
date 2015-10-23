0044111995eb6f287ab64d2b64034985137ae7c1
somename <someemail>
2015-10-23 12:37:29 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -60,7 +60,7 @@ public class PlanetExplorer {
 		System.out.println(separated.length);
 		System.out.println(separated[1]);
 		System.out.println(separated[3]);
-		System.out.println(separated[4]);
+		System.out.println(separated[5]);
 		this.explorerPositionX = Integer.parseInt(separated[1]);
 		this.explorerPositionY = Integer.parseInt(separated[3]);
 		this.explorerDirecton = separated[5];
